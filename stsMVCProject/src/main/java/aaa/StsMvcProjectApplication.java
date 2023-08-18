af6955356c775c487043e2628a6210150f5b6397
codingslave2 <codingslavekim121@gmail.com>
2023-08-18 18:27:43 +0900

    20230818

---
+++ stsMVCProject/src/main/java/aaa/StsMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package aaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsMvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsMvcProjectApplication.class, args);
+	}
+
+}
